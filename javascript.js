9ca512c4ad08063adc7831bfd92d75a741da5f84
Accellerant <accellerant@gmail.com>
2022-09-29 20:04:45 -0700

    Book Object Created

--- javascript.js
+++ javascript.js
@@ -5,3 +5,11 @@ Author: Matthew Davis
 Date Created: 29/9/2022
 Date Last Updated:
 */
+
+function Book(author, title, pages, read, favorite) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+    this.favorite = favorite;
+}

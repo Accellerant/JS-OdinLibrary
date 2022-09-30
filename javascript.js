088ac413a9f1d9b341e7c68acd26fffd6c4bd585
Accellerant <accellerant@gmail.com>
2022-09-29 20:14:17 -0700

    Array & Objects Made For Testing

--- javascript.js
+++ javascript.js
@@ -13,3 +13,11 @@ function Book(author, title, pages, read, favorite) {
     this.read = read;
     this.favorite = favorite;
 }
+
+let library = [];
+
+const book01 = new Book("R.L. Stine", 'Stay Out of the Basement', 122, true, true);
+const book02 = new Book('Dr. Seuss', 'Oh, the Places You\'ll Go!', 56, false, false);
+const book03 = new Book('Dav Pilkey', 'The Adventures of Captain Underpants', 125, true, false);
+
+library.push(book01, book02, book03);
